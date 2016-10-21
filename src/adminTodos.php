c6e28104f89fd90bbfd187faf7429e1e64eb165f
sulgie-eitea <sulgie.niedermayr@eitea.at>
2016-10-21 11:56:39 +0200

    add autocorrect function

--- src/adminTodos.php
+++ src/adminTodos.php
@@ -5,10 +5,11 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
   <link rel="stylesheet" href="../css/table.css">
+  <link rel="stylesheet" href="../css/submitButt.css">
   <link rel="stylesheet" href="../css/homeMenu.css">
 </head>
 <body>
-
+<form method=post>
 <?php
 session_start();
 if (!isset($_SESSION['userid'])) {
@@ -21,29 +22,57 @@ require 'connection.php';
 require 'createTimestamps.php';
 require 'language.php';
 
+if(isset($_POST['autoCorrect']) && isset($_POST['autoCorrects'])){
+  foreach($_POST['autoCorrects'] as $indexIM){
+    $sql = "SELECT * FROM $logTable WHERE indexIM = $indexIM";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+
+    $adjustedTime = carryOverAdder_Hours($row['time'], floor($row['expectedHours']));
+    $adjustedTime = carryOverAdder_Minutes($adjustedTime, (($row['expectedHours'] * 60) % 60));
+
+    $sql = "UPDATE $logTable SET timeEnd = '$adjustedTime' WHERE indexIM =" .$row['indexIM'];
+    $conn->query($sql);
+    echo mysqli_error($conn);
+  }
+}
 ?>
+
 <h1>Todos: </h1>
 <p>Illegal Timestamps</p>
 <table id='illTS'>
+  <th>Autocorrect</th>
   <th>User</th>
   <th><?php echo $lang['TIME']; ?></th>
+  <th><?php echo $lang['HOURS']; ?></th>
   <th>Status</th>
+
 <?php
-$sql = "SELECT $userTable.firstname, $userTable.lastname, $logTable.* FROM $logTable INNER JOIN $userTable ON $userTable.id = $logTable.userID WHERE TIMESTAMPDIFF(HOUR, time, timeEnd) > 12";
+$sql = "SELECT $userTable.firstname, $userTable.lastname, $logTable.*
+FROM $logTable
+INNER JOIN $userTable ON $userTable.id = $logTable.userID
+WHERE TIMESTAMPDIFF(HOUR, time, timeEnd) > 12 OR TIMESTAMPDIFF(HOUR, time, timeEnd) < 0";
+
 $result = $conn->query($sql);
 if($result && $result->num_rows > 0){
   while($row = $result->fetch_assoc()){
     echo '<tr>';
+    echo '<td><input type=checkbox name="autoCorrects[]" value='.$row['indexIM'].' </td>';
     echo '<td>'. $row['firstname'] .' ' . $row['lastname'] .'</td>';
     echo '<td>'. carryOverAdder_Hours($row['time'], $row['timeToUTC']) .' - ' . carryOverAdder_Hours($row['timeEnd'], $row['timeToUTC']) .'</td>';
-
+    echo '<td>'. number_format(timeDiff_Hours($row['time'], $row['timeEnd']), 2, '.', '') .'</td>';
     echo '<td>'. $lang_activityToString[$row['status']] .'</td>';
     echo '</tr>';
-
   }
 } else {
   echo mysqli_error($conn);
 }
 ?>
+
 </table>
+<br>
+<input type='submit' name='autoCorrect' value='Autocorrect'><small> - For forgotten checkouts: Set end-time to match expected hours </small></input>
+<br><br>
+<br><br>
+</form>
 </body>
