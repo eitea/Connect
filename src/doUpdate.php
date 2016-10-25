03da74dde13573ab556bf68eaddf596f34607141
sulgie-eitea <sulgie.niedermayr@eitea.at>
2016-10-25 16:09:59 +0200

    add

--- src/doUpdate.php
+++ src/doUpdate.php
@@ -82,6 +82,18 @@ if($row['version'] < 23){
       echo mysqli_error($conn);
     }
   }
+
+  $sql="SELECT userID, daysPerYear, beginningDate  FROM $userTable INNER JOIN $vacationTable ON $userTable.id = $vacationTable.userID";
+  $result = $conn->query($sql);
+  echo mysqli_error($conn);
+  while($row = $result->fetch_assoc()){
+    $time = $row['daysPerYear'] / 365;
+
+    $time *= timeDiff_Hours(substr($row['beginningDate'],0,11) .'05:00:00', substr(getCurrentTimestamp(),0,11) .'05:00:00')/24;
+
+    $sql = "UPDATE $vacationTable SET vacationHoursCredit = '$time' WHERE userID = " . $row['userID'];
+    $conn->query($sql);
+    echo mysqli_error($conn);
 }
 
 //------------------------------------------------------------------------------
