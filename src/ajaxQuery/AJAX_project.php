50923658e1c3509e2d35479844b70cc6aede1dbc
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-01-03 07:47:04 +0100

    usability+

--- src/ajaxQuery/AJAX_project.php
+++ src/ajaxQuery/AJAX_project.php
@@ -10,7 +10,9 @@ $projectID= intval($_POST['projectID']);
 $query = "SELECT * FROM $projectTable WHERE clientID = $clientID";
 $result = mysqli_query($conn, $query);
 if ($result && $result->num_rows > 0) {
-  echo "<option name='prj' value='0'> - </option>";
+  if($result->num_rows != 1){
+    echo "<option name='prj' value='0'> - </option>";
+  }
   while ($row = $result->fetch_assoc()) {
     $selected = "";
     if($projectID == $row['id']){
