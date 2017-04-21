1b44a76dcdf5cd7767e50bfdcfc04e9ea9a945a9
sulgie-eitea <sulgie@eitea.at>
2017-04-21 10:18:31 +0200

    price update

--- src/doUpdate.php
+++ src/doUpdate.php
@@ -708,13 +708,28 @@ if($row['version'] < 76){
 
 if($row['version'] < 77){
   $conn->query("DELETE p1 FROM projectData p1, projectData p2 WHERE p1.clientID = p2. clientID AND p1.name = p2.name AND p1.id > p2.id");
-
+  echo '<br> Dropped possible duplicates in projects';
   $sql = "ALTER TABLE projectData ADD UNIQUE KEY name_client (name, clientID)";
   if($conn->query($sql)){
     echo '<br> Added unqiue constraint';
   } else {
     echo mysqli_error($conn);
   }
+
+  $sql = "ALTER TABLE $companyDefaultProjectTable MODIFY COLUMN hourlyPrice DECIMAL(6,2) DEFAULT 0";
+  if($conn->query($sql)){
+    echo '<br> Increased price per hours to 4 digit number (default projects)';
+  } else {
+    echo mysqli_error($conn);
+  }
+
+  $sql = "ALTER TABLE $projectTable MODIFY COLUMN hourlyPrice DECIMAL(6,2) DEFAULT 0";
+  if($conn->query($sql)){
+    echo '<br> Increased price per hours to 4 digit number (projects)';
+  } else {
+    echo mysqli_error($conn);
+  }
+
 }
 
 //------------------------------------------------------------------------------
