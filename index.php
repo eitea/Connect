d27454c824963fd8585c38c52400d5e4a98838aa
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-07-27 16:03:21 +0200

    hail the grail

--- index.php
+++ index.php
@@ -32,15 +32,13 @@ $routes = array(
 //url must end like this:  root/ ACCESS / PAGE
 $params = explode('/', $_SERVER['REQUEST_URI']);
 $l = count($params) -1 ;
-if($l > 2){
+if($l > 1){
   $route = strtok($params[$l - 1].'/'.$params[$l], '?');
   if(array_key_exists($route, $routes)){
     $this_page = $routes[$route];
     include 'src/'.$this_page;
   } elseif($params[$l -1] == 'ajaxQuery'){
     include 'src/'.$route;
-  } elseif(in_array($params[1], array('plugins', 'images', 'bootstrap'))){
-    include $route;
   } else {
     header('HTTP/1.0 404 Not Found');
     include '404.html';
