8685fb7a1c6b5326018b204cd20dd9dd7a1aee8d
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-09-11 07:41:09 +0200

    added mime type

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $routes = array(
 $mime_types = array(
   '.css' => "text/css",                 '.js' => "text/javascript",         '.png' => "image/png",
   '.jpeg' => "image/jpeg",              '.jpg' => "image/jpg",              '.woff2' => "application/font-woff2",
-  '.gif' => "image/gif"
+  '.gif' => "image/gif",                '.woff' => "application/font-woff"
  );
 
 //kill internet explorer
@@ -52,7 +52,7 @@ if($l > 1){
   if(array_key_exists($route, $routes)){
     $this_page = $routes[$route];
     include 'src/'.$this_page;
-  } elseif(preg_match("/(images|plugins|modules)(\/.*)(\/[A-Za-z0-9\.]*)*(\.css|\.js|\.png|\.jpg|\.woff2|\.gif)$/", $url, $matches)){
+  } elseif(preg_match("/(images|plugins|modules)(\/.*)(\/[A-Za-z0-9\.]*)*(\.css|\.js|\.png|\.jpg|\.woff2|\.woff|\.gif)$/", $url, $matches)){
     if(array_key_exists($matches[4], $mime_types)){
       header('Content-Type: '. $mime_types[$matches[4]]);
     }
