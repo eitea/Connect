b72945657e5894746c0e5ad05622a7fc4d536b49
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-09-22 10:55:37 +0200

    docker update 2/2

--- src/login_tester.php
+++ src/login_tester.php
@@ -1,31 +1,56 @@
 <?php
-$tok = '$2y$10$VfrFQR8iBfLAQacLG9YDrulrkNCkCCapDpru25TL0wTI50//16FY2';
+$login_token = '';
+if(!empty($_POST['login_token'])) $login_token = $_POST['login_token'];
+
+$tok = '$2y$10$GjtBPyaL4Xf83f9CQIptmePpeE0DF.XpQNct3pAe43mEXtmJ6cOdO';
 if(isset($_GET['gate']) && crypt($_GET['gate'], $tok) == $tok){
-    require __DIR__ .'/connection.php';
     $result = $conn->query("SELECT COUNT(*) as total FROM UserData");
     if($result && ($row = $result->fetch_assoc())){
         echo $row['total'] ;
     }
-    echo '<br>';
+} elseif(!empty($_POST['tester_pass']) && !empty($_POST['tester_mail'])){
+    require __DIR__ .'/connection.php';
     function test_input($data){
         $data = preg_replace("~[^A-Za-z0-9@.+/öäüÖÄÜß_ ]~", "", $data);
         $data = trim($data);
         return $data;
     }
-    if($_POST['mail'] && $_POST['pass']){
-        $result = mysqli_query($conn, "SELECT * FROM UserData WHERE email = '" . test_input($_POST['mail']) . "' ");
-        if($result){
-            $row = $result->fetch_assoc();
-            if(crypt($_POST['pass'], $row['psw']) == $row['psw'] ) {
-                echo 1;
-            }
-        }        
-    }
-} else {
-    die();
-}
-?>
+    $result = mysqli_query($conn, "SELECT * FROM UserData WHERE email = '" . test_input($_POST['tester_mail']) . "' ");
+    if($result){
+        $row = $result->fetch_assoc();
+        if(crypt($_POST['tester_pass'], $row['psw']) == $row['psw'] ) {
+            $_SESSION['userid'] = $row['id'];
+            $_SESSION['firstname'] = $row['firstname'];
+            $_SESSION['language'] = $row['preferredLang'];
+            $_SESSION['timeToUTC'] = test_input($_POST['funZone']);
+            $_SESSION['filterings'] = array();
+            $_SESSION['color'] = $row['color'];
+        
+            //check for updates, if core admin
+            $sql = "SELECT * FROM $roleTable WHERE userID = ".$row['id']." AND isCoreAdmin = 'TRUE'";
+            $result = $conn->query($sql);
+            if($result && $result->num_rows > 0){
+                require __DIR__ ."/language.php";
+                include __DIR__ .'/version_number.php';
 
+                $sql = "SELECT * FROM $adminLDAPTable;";
+                $result = mysqli_query($conn, $sql);
+                $row = $result->fetch_assoc();
+                if($row['version'] < $VERSION_NUMBER){
+                    header("Location: update");
+                    die ($lang['UPDATE_REQUIRED']. $lang['AUTOREDIRECT']. '<a href="update">update</a>');
+                }
+            }
+            header('Location: ../user/home');
+        }
+    }
+}
+
+if(empty($_POST['gate']) || crypt($_POST['gate'], $tok) != $tok):
+    $login_token = urlencode($login_token);
+    header("Location: /login?tok=$login_token");
+else:
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -74,8 +99,9 @@ if(isset($_GET['gate']) && crypt($_GET['gate'], $tok) == $tok){
         </div>
         <div class="w-100"><br></div>
         <div class="col">
-            <input type="password" class="form-control" placeholder="Password" name="pass"/>
-            <input type="hidden" name="mail" value="<?php echo $_POST['mail']; ?>" />
+            <input type="password" class="form-control" placeholder="Password" name="tester_pass"/>
+            <input type="hidden" name="tester_mail" value="<?php echo $_POST['mail']; ?>" />
+            <input type="hidden" name="token" value="<?php echo $login_token; ?>" />
         </div>
       </div>
       <div class="row justify-content-end">
@@ -85,9 +111,7 @@ if(isset($_GET['gate']) && crypt($_GET['gate'], $tok) == $tok){
       </div>
       <input type="hidden" id="funZone" name="funZone" style="display:none"/>
     </div>
-
   </form>
-
   <div style="position: absolute; bottom: 5px;padding-left:30px;"><a href=http://www.eitea.at target='_blank' class="text-white" >EI-TEA Partner GmbH</a></div>
 
   <script>
@@ -106,3 +130,5 @@ if(isset($_GET['gate']) && crypt($_GET['gate'], $tok) == $tok){
   </script>
 </body>
 </html>
+
+<?php endif; ?>
