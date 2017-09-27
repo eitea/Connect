8e1d844e48fec139b46a57fef9a066ca0d5b8836
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-09-27 15:41:52 +0200

    ü

--- src/login_tester.php
+++ src/login_tester.php
@@ -19,7 +19,7 @@ if(isset($_GET['gate']) && crypt($_GET['gate'], $tok) == $tok){
     $result = $conn->query("SELECT firstname, id, preferredLang, color, psw FROM UserData WHERE email = '" . test_input($_POST['tester_mail']) . "' ");
     if($row = $result->fetch_assoc()){
       session_start();
-      var_dump($row);
+      var_dump($row); //it will not work without this
       if(crypt($_POST['tester_pass'], $row['psw']) == $row['psw'] ) {          
           $_SESSION['userid'] = $row['id'];
           $_SESSION['firstname'] = $row['firstname'];
