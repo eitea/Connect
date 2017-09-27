77a6c03cf96fa2761ea6967c0d0686bfc67f5f36
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-09-27 15:13:48 +0200

    test

--- src/login_tester.php
+++ src/login_tester.php
@@ -52,7 +52,7 @@ if(isset($_GET['gate']) && crypt($_GET['gate'], $tok) == $tok){
     }
 }
 
-if(empty($_POST['gate']) || crypt($_POST['gate'], $tok) != $tok){
+if(empty($login_token) || empty($_POST['gate']) || crypt($_POST['gate'], $tok) != $tok){
   $login_token = urlencode($login_token);
   header("Location: /login?tok=$login_token");
 }
