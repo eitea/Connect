b0669c947aed0186db708b3f09b0675dff9b3cd9
sulgie-eitea <sulgie.niedermayr@eitea.at>
2017-10-19 14:37:05 +0200

    article fix

--- src/ajaxQuery/AJAX_getArticles.php
+++ src/ajaxQuery/AJAX_getArticles.php
@@ -6,11 +6,12 @@ if(isset($_GET['articleID'])){
 }
 
 require dirname(__DIR__)."/connection.php";
-require dirname(__DIR__)."/encryption_functions.php";
+require dirname(__DIR__)."/utilities.php";
 
+session_start();
 $result = $conn->query("SELECT * FROM articles WHERE id = $articleID");
 $row = $result->fetch_assoc();
-$mc = mc($row["iv"],$row["iv2"]);
+$mc = new MasterCrypt($_SESSION['masterpassword'], $row["iv"],$row["iv2"]);
 $row["name"] = $mc->decrypt($row["name"]);
 $row["description"] = $mc->decrypt($row["description"]);
 echo implode('; ', $row);
