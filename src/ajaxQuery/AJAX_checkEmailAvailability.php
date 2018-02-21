2b34bcb7bf7952445932e5be054a7e033ac9c8ac
sulgie-eitea <sulgie.niedermayr@eitea.at>
2018-02-21 11:45:53 +0100

    -

--- src/ajaxQuery/AJAX_checkEmailAvailability.php
+++ src/ajaxQuery/AJAX_checkEmailAvailability.php
@@ -6,15 +6,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $mailbox = '{'.$_POST['server'] .':'. $_POST['port']. '/'.$_POST['service'] . $security .'}'.'INBOX'; //{imap.gmail.com:993/imap/ssl}INBOX ; {localhost:993/imap/ssl/novalidate-cert}
 
     if(!function_exists('imap_open')){
-        echo -1; //imap extension not installed
+        echo 'Imap not Installed'; //imap extension not installed
     } else {
         try{
             $imap = imap_open($mailbox, $username, $password, OP_READONLY, 5);
-            if($imap && ($obj = imap_check($imap))){ //imap_check will turn mails to "read" if not OP_READONLY
-                var_dump($obj);
+            if($imap && ($headers = imap_headers($imap))){ //imap_check will turn mails to "read" if not OP_READONLY
+                echo "Connection Successful!\n";
+                var_dump($headers);
                 imap_close($imap);
             } else {
-                echo 'Imap: ';
+                echo "Error in Connection\n";
                 var_dump($imap);
             }
         } catch(Exception $e) {
