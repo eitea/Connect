6d657a31ba8c619f23cb6325d6fdcc13814dd2a7
soul-eitea <34746145+soul-eitea@users.noreply.github.com>
2018-02-21 07:53:27 +0100

    Unstable

--- src/header.php
+++ src/header.php
@@ -1029,7 +1029,7 @@ $checkInButton = "<button $ckIn_disabled type='submit' class='btn btn-warning bt
           <div id="collapse-archives" class="panel-collapse collapse">
             <div class="panel-body">
               <ul class="nav navbar-nav">
-                <!--<li><a <?php //if ($this_page == 'archive_options.php') {echo $setActiveLink;}?> href="../archive/options" data-parent="#sidenav01" class="collapsed"><?php //echo $lang['OPTIONS'] ?></a></li> -->
+                <li><a <?php if ($this_page == 'archive_options.php') {echo $setActiveLink;}?> href="../archive/options" data-parent="#sidenav01" class="collapsed"><?php echo $lang['OPTIONS'] ?></a></li>
                 <li><a <?php if ($this_page == 'archive_share.php') {echo $setActiveLink;}?> href="../archive/share" data-parent="#sidenav01" class="collapsed"><?php echo $lang['SHARE'] ?></a></li>
                 <li><a <?php if ($this_page == 'private_view.php') {echo $setActiveLink;}?> href="../archive/private" data-parent="#sidenav01" class="collapsed"><?php echo $lang['PRIVATE'] ?></a></li>
               </ul>
