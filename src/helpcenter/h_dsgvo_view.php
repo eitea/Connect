e2a40db84ff4e781aad284dde02b2d083fa87abc
soul-eitea <34746145+soul-eitea@users.noreply.github.com>
2018-03-07 16:40:32 +0100

    Help DSGVO Zip-Upload

---
+++ src/helpcenter/h_dsgvo_view.php
@@ -0,0 +1,3 @@
+
+Beim Upload einer Zip-Datei ist darauf zu achten im vorhinein alles Auf Tippfehler zu überprüfen und auf Sonderzeichen im Namen zu verzichten,
+sofern die Zip-Datei nicht offiziell ist.
