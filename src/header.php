f670e95e3d715cb2da0d7101e0bab087ba426612
luke-eitea <30774283+luke-eitea@users.noreply.github.com>
2018-03-26 14:42:03 +0200

    remove vv templates from menu

--- src/header.php
+++ src/header.php
@@ -1034,7 +1034,6 @@ $checkInButton = "<button $ckIn_disabled type='submit' class='btn btn-warning bt
                   echo '<li><a href="../dsgvo/documents?n='.$available_companies[1].'">'.$lang['DOCUMENTS'].'</a></li>';
                   echo '<li><a href="../dsgvo/vv?n='.$available_companies[1].'" >'.$lang['PROCEDURE_DIRECTORY'].'</a></li>';
                   echo '<li><a href="../dsgvo/templates?n='.$available_companies[1].'">E-Mail Templates</a></li>';
-                  echo '<li><a href="../dsgvo/vtemplates?n='.$available_companies[1].'" >Ver.V. Templates</a></li>';
                   echo '<li><a href="../dsgvo/training?n='.$available_companies[1].'" >Schulung</a></li>';
                 } else {
                   $result = $conn->query("SELECT id, name FROM $companyTable WHERE id IN (".implode(', ', $available_companies).")");
@@ -1046,7 +1045,6 @@ $checkInButton = "<button $ckIn_disabled type='submit' class='btn btn-warning bt
                     echo '<li><a href="../dsgvo/documents?n='.$row['id'].'">'.$lang['DOCUMENTS'].'</a></li>';
                     echo '<li><a href="../dsgvo/vv?n='.$row['id'].'" >'.$lang['PROCEDURE_DIRECTORY'].'</a></li>';
                     echo '<li><a href="../dsgvo/templates?n='.$row['id'].'">E-Mail Templates</a></li>';
-                    echo '<li><a href="../dsgvo/vtemplates?n='.$row['id'].'" >Ver.V. Templates</a></li>';
                     echo '<li><a href="../dsgvo/training?n='.$row['id'].'" >Schulung</a></li>';
                     echo '</ul></div></li>';
                   }
