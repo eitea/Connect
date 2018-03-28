86461f299ca52df62ad69ed70193159a9cdad6f4
eitea-matt <matthias.heiden@eitea.at>
2018-03-28 14:55:51 +0200

    solved merge conflicts

--- src/core/system/social_messages.php
+++ src/core/system/social_messages.php
@@ -62,20 +62,13 @@ if (session_status() == PHP_SESSION_NONE) {
             showError($lang['MESSAGE_NOT_SPECIFIED']);
         } else {
             // no errors
-<<<<<<< HEAD
-            $to = test_input($_POST['to']);
-=======
-            $to = test_input($_POST['to']); //TODO: replace this with userID and clean with intval()
->>>>>>> master
+            $to = test_input($_POST['to']);  //TODO: replace this with userID and clean with intval()       
             $subject = test_input($_POST['subject']);
             $message = test_input($_POST['message']);
             $partnerID = -1;
 
-<<<<<<< HEAD
-=======
 
             //TODO: remove
->>>>>>> master
             // select the partnerid from the database
             $sql = "SELECT id FROM UserData WHERE concat(firstname, ' ', lastname) = '{$to}' GROUP BY id LIMIT 1";
             $result = $conn->query($sql);
@@ -154,7 +147,6 @@ if (session_status() == PHP_SESSION_NONE) {
     <div class="row">
         <div class="col-xs-4">
             <?php
-<<<<<<< HEAD
                 // the currently logged in user
                 $currentUser = $_SESSION["userid"];
 
@@ -187,109 +179,6 @@ if (session_status() == PHP_SESSION_NONE) {
                             
                             <div id="subject">
                                 <p style='padding: 10px' onclick="showChat<?php echo $subject; ?>()"><?php echo $subject; ?></h1>
-=======
-            // the currently logged in user
-            $currentUser = $_SESSION["userid"];
-            $sql = "SELECT userID, partnerID, firstname, lastname, subject FROM UserData
-                INNER JOIN messages ON messages.partnerID = UserData.id
-                WHERE userID = '{$currentUser}' or partnerID = '{$currentUser}'
-                GROUP BY subject";
-
-            $result = $conn->query($sql);
-            if ($result && $result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $name = "${row['firstname']} ${row['lastname']}";
-                    $partnerID = $row["partnerID"];
-                    $subject = $row['subject'];   /*Identify the html elements with the subject*/
-
-                    echo "<tr data-toggle='modal' data-target='#chat$subject' style='cursor:pointer;'>";
-                    echo "<td style='white-space: nowrap;width: 1%;'>$subject</td>";
-                    echo "<td style='white-space: nowrap;width: 1%;'>$name</td>";
-
-                    echo '</tr>';
-                    ?>
-
-                    <!-- chat modal -->
-                    <div class="modal fade" id="chat<?php echo $subject; ?>" tabindex="-1" role="dialog"
-                        aria-labelledby="chatLabel<?php echo $subject; ?>">
-                        <div class="modal-dialog" role="form">
-
-                            <div class="modal-content">
-                                <!-- Title -->
-                                <div class="modal-header" style="padding-bottom:5px;">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-
-                                    <h4 class="modal-title" id="chatLabel<?php echo $subject; ?>">
-                                        <img src='<?php echo $profilePicture; ?>' alt='Profile picture' class='img-circle'
-                                            style='width:25px;display:inline-block;'> <?php echo $name ?>
-                                    </h4>
-                                </div>
-
-                                <br>
-
-                                <!-- All messages -->
-                                <div class="modal-body">
-                                    <div id="messages<?php echo $subject; ?>">
-                                        <?php
-                                            // select all messages
-                                            $sql = "SELECT message, firstname, lastname FROM UserData
-                                                INNER JOIN messages ON messages.partnerID = UserData.id
-                                                WHERE subject = '{$subject}'
-                                                ORDER BY sent ASC";
-
-                                            $result = $conn->query($sql);
-                                            if ($result && $result->num_rows > 0) {
-                                                while ($row = $result->fetch_assoc()) {
-                                                    $message = $row['message'];
-                                                    $name = "${row['firstname']} ${row['lastname']}";
-                                                    ?>
-
-                                                    <!-- The message -->
-                                                    <div class='row'>
-                                                        <div class='col-xs-12'>
-                                                            <div class='well <?php echo $pull; ?>' style='position:relative'>
-                                                                <i class="fa <?php echo $seen; ?>" style="display:block;top:0px;right:-3px;position:absolute;color:#9d9d9d;"></i>
-                                                                <span class="label label-default" style="display:block;top:-17px;left:0px;position:absolute;"><?php echo $name; ?></span>
-                                                                <div><?php echo $message ?></div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <?php
-                                                }
-                                            }
-                                        ?>
-                                    </div>
-                                </div>
-
-                                <!-- AJAX stuff -->
-                                <!--<script>
-                                    interval<?php echo $subject; ?> = 0
-                                    limit<?php echo $subject; ?> = 10
-                                    $("#chat<?php echo $subject; ?>").on('show.bs.modal', function (e) {
-                                        getMessages(<?php echo $subject; ?>, "#messages<?php echo $subject; ?>", true, limit<?php echo $subject; ?>)
-                                        interval<?php echo $subject; ?> = setInterval(function () {
-                                            getMessages(<?php echo $subject; ?>, "#messages<?php echo $subject; ?>", false, limit<?php echo $subject; ?>)
-                                        }, 1000)
-                                    })
-
-                                    // scroll
-                                    $("#messages<?php echo $subject; ?>").parent().scroll(function () {
-                                        if ($("#messages<?php echo $subject; ?>").parent().scrollTop() == 0) {
-                                            limit<?php echo $subject; ?> += 1
-                                            $("#messages<?php echo $subject; ?>").parent().scrollTop(1);
-                                            getMessages(<?php echo $subject; ?>, "#messages<?php echo $subject; ?>", false, limit<?php echo $subject; ?>)
-                                        }
-                                    })
-
-                                    // scroll to top
-                                    $("#chat<?php echo $subject; ?>").on('shown.bs.modal', function (e) {
-                                        $("#messages<?php echo $subject; ?>").parent().scrollTop($("#messages<?php echo $subject; ?>")[0].scrollHeight);
-                                    })-->
-                                </script>
->>>>>>> master
                             </div>
                             
                             <!-- Make the div visible, when someone clicks the button -->
