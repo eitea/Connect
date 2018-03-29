78c5272c7e627ed6280445213573af27fdf51d3e
luke-eitea <30774283+luke-eitea@users.noreply.github.com>
2018-03-29 09:50:29 +0200

    improved log

--- src/dsgvo/dsgvo_log.php
+++ src/dsgvo/dsgvo_log.php
@@ -8,23 +8,23 @@ enableToDSGVO($userID);
         <h3>Logs</h3>
     </div>
 
-    <table class="table">
+    <table class="table table-hover">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Benutzer</th>
-                <th>Beschreibung</th>
-                <th>Lange Beschreibung</th>
                 <th>Uhrzeit</th>
+                <th>Beschreibung</th>
+                <th>Benutzer</th>
+                <th>Lange Beschreibung</th>
             </tr>
         </thead>
         <tbody>
             <?php
-$result = $conn->query("SELECT * FROM dsgvo_vv_logs ORDER BY log_time DESC");
+$result = $conn->query("SELECT dsgvo_vv_logs.*,UserData.firstname,UserData.lastname, TIMEDIFF(dsgvo_vv_logs.log_time,CURRENT_TIMESTAMP) AS timespan FROM dsgvo_vv_logs, UserData WHERE UserData.id = dsgvo_vv_logs.user_id ORDER BY log_time DESC,id DESC");
 showError($conn->error);
 while ($result && ($row = $result->fetch_assoc())) {
     $id = $row["id"];
     $user_id = $row["user_id"];
+    $user_name = $row["firstname"]." ".$row["lastname"];
     try{
         $short_description = $row["short_description"];
         $short_description = secure_data('DSGVO', $row["short_description"], 'decrypt', $userID, $privateKey, $encryptionError);
@@ -35,16 +35,43 @@ while ($result && ($row = $result->fetch_assoc())) {
         showError($e->getMessage());
     }
     $log_time = $row["log_time"];
-    echo "<tr>";
-    echo "<td>$id</td>";
-    echo "<td>$user_id</td>";
-    echo "<td>$short_description</td>";
+    $timediff = $row["timespan"];
+    $tr_classes = "";
+    $short_description_classes = "";
+    switch($short_description){
+        case "INSERT": 
+            $tr_classes = "bg-success"; 
+            $short_tooltip = "Ein neuer Eintrag wurde hinzugefügt";
+            $short_description_classes = "text-success text-bold";
+            break;
+        case "UPDATE": 
+            $tr_classes = "bg-warning"; 
+            $short_tooltip = "Ein bestehender Eintrag wurde verändert";
+            $short_description_classes = "text-warning";
+            break;
+        case "DELETE": 
+            $tr_classes = "bg-danger"; 
+            $short_tooltip = "Ein bestehender Eintrag wurde entfernt";
+            $short_description_classes = "text-danger";
+            break;
+        default:
+            $tr_classes = "";
+            $short_tooltip = "";
+            $short_description_classes = "text-muted";
+    }
+    echo "<tr class='$tr_classes'>";
+    echo "<td data-toggle='tooltip' data-container='body' data-placement='right' title='$timediff (Log ID: $id)'>$log_time</td>";
+    echo "<td class='$short_description_classes' data-toggle='tooltip' data-container='body' data-placement='right' title='$short_tooltip'><strong>$short_description</strong></td>";
+    echo "<td data-toggle='tooltip' data-container='body' data-placement='right' title='Benutzer ID: $user_id'>$user_name</td>";
     echo "<td>$long_description</td>";
-    echo "<td>$log_time</td>";
     echo "</tr>";
 }
 ?>
         </tbody>
     </table>
+
+<script>
+$('[data-toggle="tooltip"]').tooltip(); 
+</script>
 
     <?php include dirname(__DIR__) . '/footer.php';?>
